5cbb24a48c0109a94a6247f20e4f32345f0b077f
Trang Do <trang@codevolve.com>
2018-11-13 16:27:06 -0500

    Create main.cpp

---
+++ math-problems/04-largest-prime-smaller-than-given-number/starter/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+bool is_prime(int const num) 
+{
+   
+}
+
+int main()
+{
+   int limit = 0;
+   std::cout << "Upper limit:";
+   std::cin >> limit;
+
+   for (int i = limit; i > 1; i--)
+   {
+      if (is_prime(i))
+      {
+         std::cout << "Largest prime:" << i << std::endl;
+         return 0;
+      }
+   }
+}
