2452a7bfcd8bd5b7be98a0ce259905983b4ff296
Trang Do <trang@codevolve.com>
2018-11-13 16:03:56 -0500

    Create main.cpp

---
+++ math-problems/02-greatest-common-divisor/solution/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+unsigned int gcd(unsigned int a, unsigned int b)
+{
+   while (b != 0) {
+      unsigned int r = a % b;
+      a = b;
+      b = r;
+   }
+   return a;
+}
+
+int main()
+{
+    unsigned int a, b;
+    cout << "Value for 'a': ";
+    cin >> a;
+    
+    cout << "Value for 'b': "
+    cin >> b;
+    
+    cout << "gcd = " << gcd(a,b);
+ }
