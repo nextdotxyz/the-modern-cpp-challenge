511ba4f292dc197f7e15a5388dcdbbfce55fa14a
Trang Do <trang@codevolve.com>
2019-01-27 23:17:33 -0500

    Create main.cpp

---
+++ math-problems/08-armstrong-numbers/solution/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void print_narcissistics()
+{
+   for (int a = 1; a <= 9; a++)
+   {
+      for (int b = 0; b <= 9; b++)
+      {
+         for (int c = 0; c <= 9; c++)
+         {
+            auto abc = a * 100 + b * 10 + c;
+            auto arm = a * a * a + b * b * b + c * c * c;
+            if (abc == arm)
+            {
+               std::cout << arm << std::endl;
+            }
+         }
+      }
+   }
+}
+
+int main()
+{
+   print_narcissistics();
+}
