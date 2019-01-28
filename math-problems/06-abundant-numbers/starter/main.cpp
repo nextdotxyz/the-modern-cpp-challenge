eab708d94dd24f3f2b6b001a39b28ba1783222df
Trang Do <trang@codevolve.com>
2019-01-27 23:00:16 -0500

    Create main.cpp

---
+++ math-problems/06-abundant-numbers/starter/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+int sum_proper_divisors(int const number)
+{
+   
+}
+
+void print_abundant(int const limit)
+{
+   
+}
+
+int main()
+{
+   int limit = 0;
+   std::cout << "Upper limit:";
+   std::cin >> limit;
+
+   print_abundant(limit);
+}
