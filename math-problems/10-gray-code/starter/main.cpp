76176560504e650ad9a2d799b698d8fa5e2e31ee
Trang Do <trang@codevolve.com>
2019-02-12 16:06:17 -0500

    Update main.cpp

--- math-problems/10-gray-code/starter/main.cpp
+++ math-problems/10-gray-code/starter/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
 
 unsigned int gray_encode(unsigned int const num)
 {
@@ -10,22 +13,22 @@ unsigned int gray_decode(unsigned int gray)
    
 }
 
-std::string to_binary(unsigned int value, int const digits)
+string to_binary(unsigned int value, int const digits)
 {
 
 }
 
 int main()
 {
-   std::cout << "Number\tBinary\tGray\tDecoded\n";
-   std::cout << "------\t------\t----\t-------\n";
+   cout << "Number\tBinary\tGray\tDecoded\n";
+   cout << "------\t------\t----\t-------\n";
 
    for (unsigned int n = 0; n < 32; ++n)
    {
       auto encg = gray_encode(n);
       auto decg = gray_decode(encg);
 
-      std::cout 
+      cout 
          << n << "\t" << to_binary(n, 5) << "\t" 
          << to_binary(encg, 5) << "\t" << decg << "\n";
    }
