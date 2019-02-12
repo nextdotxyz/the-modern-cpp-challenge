a0b8fade16a4a28fd99efb814e583fc99a27e1f7
Trang Do <trang@codevolve.com>
2019-02-12 11:11:54 -0500

    Update main.cpp

--- math-problems/01-sum-of-naturals-divisible-by-3-and-5/starter/main.cpp
+++ math-problems/01-sum-of-naturals-divisible-by-3-and-5/starter/main.cpp
@@ -2,5 +2,12 @@
 
 int main()
 {
-  
+   unsigned int limit = 0;
+   std::cout << "Upper limit:";
+   std::cin >> limit;
+
+   unsigned long long sum = 0;
+   
+
+   std::cout << "sum=" << sum << std::endl;
 }
