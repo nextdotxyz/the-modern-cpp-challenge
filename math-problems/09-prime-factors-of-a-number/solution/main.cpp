bde91bacaa1bdccb4b20af392a2d5a9a5babeec6
Trang Do <trang@codevolve.com>
2019-02-12 16:04:18 -0500

    Update main.cpp

--- math-problems/09-prime-factors-of-a-number/solution/main.cpp
+++ math-problems/09-prime-factors-of-a-number/solution/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <math.h>
+using namespace std;
 
-std::vector<unsigned long long> prime_factors(unsigned long long n)
+vector<unsigned long long> prime_factors(unsigned long long n)
 {
-   std::vector<unsigned long long> factors;
+   vector<unsigned long long> factors;
    while (n % 2 == 0) {
       factors.push_back(2);
       n = n / 2;
    }
-   for (unsigned long long i = 3; i <= std::sqrt(n); i += 2)
+   for (unsigned long long i = 3; i <= sqrt(n); i += 2)
    {
       while (n%i == 0) {
          factors.push_back(i);
@@ -23,9 +25,9 @@ std::vector<unsigned long long> prime_factors(unsigned long long n)
 int main()
 {
    unsigned long long number = 0;
-   std::cout << "number:";
-   std::cin >> number;
+   cout << "number:";
+   cin >> number;
    auto factors = prime_factors(number);
-   std::copy(std::begin(factors), std::end(factors),
-   std::ostream_iterator<unsigned long long>(std::cout, " "));
+   copy(std::begin(factors), end(factors),
+   ostream_iterator<unsigned long long>(cout, " "));
 }
