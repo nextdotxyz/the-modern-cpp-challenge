6e429248af2212d9635d7008b3980b4949b0cfba
Trang Do <trang@codevolve.com>
2019-02-12 22:59:13 -0500

    Update main.cpp

--- math-problems/11-converting-numerical-values-to-roman/solution/main.cpp
+++ math-problems/11-converting-numerical-values-to-roman/solution/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
-std::string to_roman(unsigned int value)
+string to_roman(unsigned int value)
 {
-   std::vector<std::pair<unsigned int, char const*>> roman {
+   vector<pair<unsigned int, char const*>> roman {
       { 1000, "M" },{ 900, "CM" }, { 500, "D" },{ 400, "CD" }, 
       { 100, "C" },{ 90, "XC" }, { 50, "L" },{ 40, "XL" },
       { 10, "X" },{ 9, "IX" }, { 5, "V" },{ 4, "IV" }, { 1, "I" }};
 
-   std::string result;
+   string result;
    for (auto const & kvp : roman) {
       while (value >= kvp.first) {
          result += kvp.second;
@@ -21,11 +24,11 @@ int main()
 {
    for(int i = 1; i <= 100; ++i) 
    {
-      std::cout << i << "\t" << to_roman(i) << std::endl; 
+      cout << i << "\t" << to_roman(i) << endl; 
    }
 
    int number = 0;
-   std::cout << "number:";
-   std::cin >> number;
-   std::cout << to_roman(number) << std::endl;
+   cout << "number:";
+   cin >> number;
+   cout << to_roman(number) << endl;
 }
