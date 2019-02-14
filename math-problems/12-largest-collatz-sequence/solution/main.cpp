ec8b5e904fd4b296e1bdaa228c91b8459db55be0
Trang Do <trang@codevolve.com>
2019-02-13 21:17:05 -0500

    Update main.cpp

--- math-problems/12-largest-collatz-sequence/solution/main.cpp
+++ math-problems/12-largest-collatz-sequence/solution/main.cpp
@@ -34,7 +34,7 @@ pair<unsigned long long, long> longest_collatz(unsigned long long const limit)
 ostream& operator<<(ostream& os, const pair<unsigned long long, long>& obj)
 {
   // write obj to stream
-  os << "Length: " << obj.second << \t << "Starting Num: " << obj.first << endl;
+  os << "Length: " << obj.second << "\t" << "Starting Num: " << obj.first << endl;
   return os;
 }
 
