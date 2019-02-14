0e548883bdac13cab845a0063583711931d408a9
Trang Do <trang@codevolve.com>
2019-02-13 22:17:45 -0500

    Update main.cpp

--- math-problems/12-largest-collatz-sequence/starter/main.cpp
+++ math-problems/12-largest-collatz-sequence/starter/main.cpp
@@ -8,6 +8,13 @@ std::pair<unsigned long long, long> longest_collatz(unsigned long long const lim
   
 }
 
+ostream& operator<<(ostream& os, const pair<unsigned long long, long>& obj)
+{
+  // write obj to stream
+  os << "Length: " << obj.second << "\t" << "Starting Num: " << obj.first << endl;
+  return os;
+}
+
 int main()
 {
    std::cout << longest_collatz(1000000);
