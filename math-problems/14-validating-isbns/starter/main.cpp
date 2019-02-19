f1657e8e476fa89ee5771c59019146bb3469ebf9
Trang Do <trang@codevolve.com>
2019-02-18 23:39:57 -0500

    Update main.cpp

--- math-problems/14-validating-isbns/starter/main.cpp
+++ math-problems/14-validating-isbns/starter/main.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
+#include <string>
+#include <algorithm> 
+#include <numeric>
+using namespace std;
 
-bool validate_isbn_10(std::string_view isbn)
+bool validate_isbn_10(string isbn)
 {
-   
+  
 }
 
 int main()
 {
-   std::string_view input = "";
-   std::cout << "ISBN: ";
-   std::cin >> input;
+   string input = "";
+   cout << "ISBN: ";
+   cin >> input;
    
    bool valid = validate_isbn_10(input);
    
    if(valid)
    {
-     std::cout << input << " is a valid ISBN.";
+     cout << input << " is a valid ISBN.";
    }
    else
    {
-     std::cout << input << " is not a valid ISBN.";
+     cout << input << " is not a valid ISBN.";
    }
 }
+
