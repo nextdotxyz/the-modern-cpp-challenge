3ae49140f8bd049df9b01b70bfd59bb1fba3bc8b
Trang Do <trang@codevolve.com>
2019-02-18 22:44:01 -0500

    Update main.cpp

--- math-problems/13-computing-the-value-of-pi/starter/main.cpp
+++ math-problems/13-computing-the-value-of-pi/starter/main.cpp
@@ -2,6 +2,9 @@
 #include <math.h>
 #include <random>
 #include <chrono>
+#include <array>
+#include <algorithm>
+#include <cstdlib>
 using namespace std;
 
 template <typename E = mt19937, 
@@ -9,15 +12,15 @@ template <typename E = mt19937,
           
 double compute_pi(E& engine, D& dist, int const samples = 1000000)
 {
-  
+
 }
+
+int RandomNumber () { return (rand()%100); }
 
 int main()
 {
-   random_device rd;
    auto seed_data = array<int, mt19937::state_size> {};
-   generate(:begin(seed_data), end(seed_data), 
-                 ref(rd));
+   generate(begin(seed_data), end(seed_data), RandomNumber);
    seed_seq seq(begin(seed_data), end(seed_data));
    auto eng = mt19937{ seq };
    auto dist = uniform_real_distribution<>{ 0, 1 };
