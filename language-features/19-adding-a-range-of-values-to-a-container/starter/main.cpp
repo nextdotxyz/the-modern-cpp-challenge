fb23991a0100a5e360384b400e1cc018a48c5058
Luke Schenk <41804945+LukeDSchenk@users.noreply.github.com>
2019-02-28 15:40:11 -0500

    created starter file

---
+++ language-features/19-adding-a-range-of-values-to-a-container/starter/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <iterator>
+#include <list>
+using namespace std;
+
+template<typename C, typename... Args>
+void push_back(C& c, Args&&... args)
+{
+   // your code here
+}
+
+int main()
+{
+   vector<int> v;
+   push_back(v, 1, 2, 3, 4);
+   copy(begin(v), end(v), ostream_iterator<int>(cout, " "));
+
+   list<int> l;
+   push_back(l, 1, 2, 3, 4);   
+   copy(begin(l), end(l), ostream_iterator<int>(cout, " "));
+}
