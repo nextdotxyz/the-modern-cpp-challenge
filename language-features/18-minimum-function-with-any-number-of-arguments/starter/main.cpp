ae18eea360790d71d74533590fd114637658e0fc
Luke Schenk <41804945+LukeDSchenk@users.noreply.github.com>
2019-03-06 20:57:53 -0500

    updated to match tests

--- language-features/18-minimum-function-with-any-number-of-arguments/starter/main.cpp
+++ language-features/18-minimum-function-with-any-number-of-arguments/starter/main.cpp
@@ -3,19 +3,13 @@
 using namespace std;
 
 template <typename T>
-T minimum(T const a, T const b) { return a < b ? a : b; }
+T minimum(T const a, T const b) 
+{
+    // your code here 
+}
 
 template <typename T1, typename... T>
 T1 minimum(T1 a, T... args)
-{
-   // your code here
-}
-
-template <class Compare, typename T>
-T minimumc(Compare comp, T const a, T const b) { return comp(a, b) ? a : b; }
-
-template <class Compare, typename T1, typename... T>
-T1 minimumc(Compare comp, T1 a, T... args)
 {
    // your code here
 }
@@ -25,6 +19,9 @@ int main()
    auto x = minimum(5, 4, 2, 3);
    cout << x << endl;
    
-   auto y = minimumc(less<>(), 3, 2, 1, 0);
+   auto y = minimum(6, 7, 7, 8, 4, 6, 9, 46, 4, 4, 33, 9);
    cout << y << endl;
+    
+   auto z = minimum(434, 221);
+   cout << z << endl;
 }
