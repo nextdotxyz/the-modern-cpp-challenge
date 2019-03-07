5fa3c606c4b2586cd423f01c90101486b16b20b5
Luke Schenk <41804945+LukeDSchenk@users.noreply.github.com>
2019-03-06 20:57:04 -0500

    Update main.cpp

--- language-features/18-minimum-function-with-any-number-of-arguments/solution/main.cpp
+++ language-features/18-minimum-function-with-any-number-of-arguments/solution/main.cpp
@@ -11,20 +11,14 @@ T1 minimum(T1 a, T... args)
    return minimum(a, minimum(args...));
 }
 
-template <class Compare, typename T>
-T minimumc(Compare comp, T const a, T const b) { return comp(a, b) ? a : b; }
-
-template <class Compare, typename T1, typename... T>
-T1 minimumc(Compare comp, T1 a, T... args)
-{
-   return minimumc(comp, a, minimumc(comp, args...));
-}
-
 int main()
 {
    auto x = minimum(5, 4, 2, 3);
    cout << x << endl;
    
-   auto y = minimumc(less<>(), 3, 2, 1, 0);
+   auto y = minimum(6, 7, 7, 8, 4, 6, 9, 46, 4, 4, 33, 9);
    cout << y << endl;
+    
+   auto z = minimum(434, 221);
+   cout << z << endl;
 }
