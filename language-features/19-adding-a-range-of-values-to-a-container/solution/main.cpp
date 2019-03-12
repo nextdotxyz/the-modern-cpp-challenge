d5afc86964e9d5466e4f49a9d1a447d74afcbe2e
Luke Schenk <41804945+LukeDSchenk@users.noreply.github.com>
2019-03-11 20:50:47 -0400

    Update main.cpp

--- language-features/19-adding-a-range-of-values-to-a-container/solution/main.cpp
+++ language-features/19-adding-a-range-of-values-to-a-container/solution/main.cpp
@@ -16,8 +16,16 @@ int main()
    vector<int> v;
    push_back(v, 1, 2, 3, 4);
    copy(begin(v), end(v), ostream_iterator<int>(cout, " "));
+   
+   cout << endl;
 
    list<int> l;
-   push_back(l, 1, 2, 3, 4);   
+   push_back(l, 43, 432, 2, 4, 56);   
    copy(begin(l), end(l), ostream_iterator<int>(cout, " "));
+    
+   cout << endl;
+    
+   list<int> x;
+   push_back(x, 6, 4, 3, 25, 6, 2, 2, 4, 3, 2, 53, 7);
+   copy(begin(x), end(x), ostream_iterator<int>(cout, " "));
 }
